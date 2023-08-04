ea1ea5037e4beba9e7131060dafbd2c34df80ec5
Latsa <bgkcsavvy@gmail.com>
2023-08-04 14:14:07 +0200

    done

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -3,7 +3,19 @@ package com.epam.mjc.collections.list;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            if ((i + 1) % 3 == 0) {
+                String word = sourceList.get(i);
+                result.add(word);
+                result.add(word);
+            }
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,32 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        ListComparator comparator = new ListComparator();
+        Collections.sort(sourceList, comparator);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int numA = Integer.parseInt(a);
+        int numB = Integer.parseInt(b);
+
+        int result = calculateFunction(numA) - calculateFunction(numB);
+
+        if (result != 0) {
+            return result;
+        } else {
+            return numA - numB;
+        }
+    }
+
+    private int calculateFunction(int x) {
+        return 5 * x * x + 3;
     }
 }
